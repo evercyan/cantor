309c351d40e7b930022ff011dd2264a80ed02bb6
Ryan <evercyan@qq.com>
2020-07-23 10:45:41 +0800

    cantor auto deploy

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 ps aux | grep 'cantor/src/main.py' | grep -v 'grep' | awk '{print $2}' | xargs kill -9
 
 path=$(cd "$(dirname "$0")"; pwd)
-nohup python3 "${path}/src/main.py" &
+nohup python3 "${path}/src/main.py" >/dev/null 2>&1 &
