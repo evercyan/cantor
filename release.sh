c84fd3b63cb4fbe8102035d15304f1176314a5e1
Cyan <evercyan@qq.com>
2020-08-31 18:30:48 +0800

    release.sh

--- release.sh
+++ release.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-service=`basename $PWD`
-version=$1
-if [ "${version}" == "" ]; 
-then
-    echo "invalid version";
-	exit;
-fi
-
-rm -rf "./build/${service}.app"
-rm -rf "./dist/${service}.app"
+app=`basename $PWD`
+path="./build/${app}.app"
+file="./build/${app}.tar.gz"
+rm -rf $path
 wails build -p 
-mv "./build/${service}.app" "./dist/${service}.app"
-
-git add . && git commit -m $version && git push origin master
-git tag -a $version -m $version
-git push origin $version
+tar zcvf $file $path
