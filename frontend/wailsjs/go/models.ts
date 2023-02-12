a55c5f33fa603c96fc321f203f00bf8351b1f381
Ryan <evercyan@qq.com>
2023-02-12 19:53:30 +0800

    wails v2.3.1

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -1,37 +1,21 @@
-/* Do not change, this code is generated from Golang structs */
+export namespace internal {
+	
+	export class Response {
+	    code: number;
+	    msg: string;
+	    data: any;
+	
+	    static createFrom(source: any = {}) {
+	        return new Response(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.code = source["code"];
+	        this.msg = source["msg"];
+	        this.data = source["data"];
+	    }
+	}
 
-export {};
-
-export class Response {
-    code: number;
-    msg: string;
-    data: any;
-
-    static createFrom(source: any = {}) {
-        return new Response(source);
-    }
-
-    constructor(source: any = {}) {
-        if ('string' === typeof source) source = JSON.parse(source);
-        this.code = source["code"];
-        this.msg = source["msg"];
-        this.data = source["data"];
-    }
-}
-
-
-
-
-export class Menu {
-
-
-    static createFrom(source: any = {}) {
-        return new Menu(source);
-    }
-
-    constructor(source: any = {}) {
-        if ('string' === typeof source) source = JSON.parse(source);
-
-    }
 }
 
